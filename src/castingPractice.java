805ba60e77bd1fcd3f8916290b18a773f716b89d
janvinaidu <janvinnaidu@gmail.com>
2025-08-26 08:47:42 -0700

    Update castingPractice.java

--- src/castingPractice.java
+++ src/castingPractice.java
@@ -3,6 +3,8 @@ public class castingPractice {
     public static void main(String[] args)
     {
         // Enter a double 11.09
+        double num1 = 11.09;
+        int num2 = (int) num1;
 
         //11.09 cast to an int is 11
     }
